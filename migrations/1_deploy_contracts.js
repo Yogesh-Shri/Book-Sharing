271a0ca9452a37acf19008c1c5ae9ae2b5c2390f
Yogesh-Shri <yogesh160496@gmail.com>
2023-05-20 12:02:07 +0530

    openZeppelin imported

---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,6 @@
+const BookSharingPlatform = artifacts.require("LibraryBooks");
+
+module.exports = (deployer, network, accounts) => {
+  const verifierAdd = accounts[0];
+  deployer.deploy(BookSharingPlatform, verifierAdd);
+};
