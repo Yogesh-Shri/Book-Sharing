72403208dd3eae4d5a50b8f7438f7c4f4def143c
Yogesh-Shri <yogesh160496@gmail.com>
2023-05-24 13:25:46 +0530

    contracted Deployed again

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -2,5 +2,5 @@ const BookSharingPlatform = artifacts.require("LibraryBooks");
 
 module.exports = (deployer, network, accounts) => {
   const verifierAdd = accounts[0];
-  deployer.deploy(BookSharingPlatform, verifierAdd);
+  deployer.deploy(BookSharingPlatform);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -41,10 +41,10 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -64,11 +64,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -83,7 +83,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     mumbai: {
-      provider: () => new HDWalletProvider(MNEMONIC, `wss://polygon-mumbai.g.alchemy.com/v2/CZg6H5TlpZV_4s-eDAGhP44Kgzf99fi1`),
+      provider: () => new HDWalletProvider("start unit volume shoot lawsuit square kingdom car rack barrel tent kangaroo", `wss://polygon-mumbai.g.alchemy.com/v2/CZg6H5TlpZV_4s-eDAGhP44Kgzf99fi1`),
       network_id: 80001,       // Goerli's id
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
